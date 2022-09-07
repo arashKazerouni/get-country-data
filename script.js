90b11f382e3244c4599074171b04394d71f4b9f0
Arash Kazerouni <arashkazerooni484@gmail.com>
2022-09-07 15:56:52 +0430

    code gets so much cleaner using fetch and two promises

--- script.js
+++ script.js
@@ -28,45 +28,21 @@ const renderCountry = function (data, ClassName = '') {
   countriesContainer.style.opacity = 1;
 };
 
-// Ajax Call
-const ajaxCall = () => {};
-
-// Get Country And neighbour
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country (1)
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-
-    // render country (1)
-    renderCountry(data);
-
-    // Get neighbour country (2)
-    const [neighbour] = data.borders;
-
-    if (!neighbour) return;
-
-    // AJAX call country (2)
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-
-      // render country (2)
-      renderCountry(data2, 'neighbour');
-    });
-  });
+const getCountryData = country => {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]));
 };
+// getCountryData('portugal');
 
 // Click Handler
 document.addEventListener('click', e => {
   e.preventDefault();
   const isButton = e.target === button;
   if (isButton) {
-    getCountryAndNeighbour(input.value);
+    getCountryData(input.value);
     input.value = '';
   }
 });
