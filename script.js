d9488546aca05a6dbcf828c81d688c82f43d2b74
Arash Kazerouni <arashkazerooni484@gmail.com>
2022-09-07 14:40:41 +0430

    wellcome to Callback Hell

--- script.js
+++ script.js
@@ -8,44 +8,70 @@ const errorText = document.getElementById('errorText');
 input.value = '';
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
+// Render Country From Data
+const renderCountry = function (data, ClassName = '') {
+  const html = `
+  <article class="country ${ClassName}">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+  </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// Ajax Call
+const ajaxCall = () => {};
+
+// Get Country And neighbour
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country (1)
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    const html = `
-          <article class="country">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-              <h3 class="country__name">${data.name}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(
-                +data.population / 1000000
-              ).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️</span>${
-                data.languages[0].name
-              }</p>
-              <p class="country__row"><span>💰</span>${
-                data.currencies[0].name
-              }</p>
-          </div>
-          </article>
-        `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-    errorText.innerText = '';
+
+    // render country (1)
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country (2)
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+
+      // render country (2)
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-const loadCountry = e => {
+
+// Click Handler
+document.addEventListener('click', e => {
   e.preventDefault();
   const isButton = e.target === button;
   if (isButton) {
-    getCountryData(input.value);
+    getCountryAndNeighbour(input.value);
     input.value = '';
   }
-};
-document.addEventListener('click', loadCountry);
+});
+
+// Enter Handler
 input.addEventListener('keypress', function (event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === 'Enter') {
